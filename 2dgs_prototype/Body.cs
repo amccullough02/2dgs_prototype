118297b1f1268c90fb91f2f3cee674a01ba35ce3
Alex McCullough <alexmccullough@outlook.com>
2024-11-15 21:20:59 +0000

    feat: rendering bodies

--- 2dgs_prototype/Body.cs
+++ 2dgs_prototype/Body.cs
@@ -1,24 +1,42 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace _2dgs_prototype;
 
 public class Body
 {
+    private Texture2D Texture;
     private Vector2 Position;
     private Vector2 Velocity;
     private int Mass;
     private int Radius;
 
-    public Body(Vector2 position, Vector2 velocity, int mass, int radius)
+    public Body(Vector2 position, int mass, int radius)
     {
         this.Position = position;
-        this.Velocity = velocity;
+        this.Velocity = Vector2.Zero;
         this.Mass = mass;
         this.Radius = radius;
     }
+
+    public void LoadContent(ContentManager content)
+    {
+        Texture = content.Load<Texture2D>("blank_circle");
+    }
     
-    // Method stubs
-    public void Draw() {}
-    
-    public void Update(float deltaTime) {}
+    public void Update(GameTime gameTime) {}
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(Texture, 
+            Position,
+            null,
+            Color.White,
+            0f,
+            new Vector2(Texture.Width / 2, Texture.Height / 2),
+            new Vector2(0.1f, 0.1f),
+            SpriteEffects.None,
+            0f);
+    }
 }
--- 2dgs_prototype/Game.cs
+++ 2dgs_prototype/Game.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace _2dgs_prototype;
@@ -6,6 +8,11 @@ namespace _2dgs_prototype;
 public class Game : Microsoft.Xna.Framework.Game
 {
     private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    
+    private List<Body> _bodies;
+    private Body body_one;
+    private Body body_two;
 
     public Game()
     {
@@ -16,15 +23,32 @@ public class Game : Microsoft.Xna.Framework.Game
         
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+        
+        _bodies = new List<Body>();
+
+        body_one = new Body(new Vector2(300.0f, 300.0f),1, 1);
+        _bodies.Add(body_one);
+        
+        body_two = new Body(new Vector2(1300.0f, 900.0f), 1, 1);
+        _bodies.Add(body_two);
     }
 
     protected override void Initialize()
     {
         Window.Title = "2DGS";
+        
         base.Initialize();
     }
 
-    protected override void LoadContent() {}
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        foreach (Body body in _bodies)
+        {
+            body.LoadContent(Content);
+        }
+    }
 
     protected override void Update(GameTime gameTime)
     {
@@ -37,6 +61,14 @@ public class Game : Microsoft.Xna.Framework.Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.Black);
+        
+        _spriteBatch.Begin();
+        foreach (Body body in _bodies)
+        {
+            body.Draw(_spriteBatch);
+        }
+        _spriteBatch.End();
+        
         base.Draw(gameTime);
     }
 }
